049cd589ad40417c40727e080cb59cff10c96278
firewood <karamaki@gmail.com>
2011-04-21 02:13:57 +0900

    add integer test

--- serialize_bench.php
+++ serialize_bench.php
@@ -11,7 +11,15 @@ function generate_random_string($length) {
     return $result;
 }
 
-function generate_array($keys, $length) {
+function generate_random_integer_array($keys, $max) {
+    $result = array();
+    while (--$keys >= 0) {
+        $result[] = mt_rand(0, $max);
+    }
+    return $result;
+}
+
+function generate_random_string_array($keys, $length) {
     $result = array();
     while (--$keys >= 0) {
         $key = generate_random_string(10);
@@ -24,47 +32,71 @@ function test_serialize($a) {
     print("test_serialize: ");
     for ($i = 0; $i < 1000; ++$i) {
         $s = serialize($a);
-        $u = unserialize($s);
     }
+    return $s;
 }
 
-function test_json($a) {
-    print("test_json: ");
+function test_unserialize($s) {
+    print("test_unserialize: ");
     for ($i = 0; $i < 1000; ++$i) {
-        $s = json_encode($a);
-        $u = json_decode($s, True);
+        $a = unserialize($s);
     }
+    return $a;
 }
 
-function test_var_export($a) {
-    print("test_var_export: ");
+function test_json_encode($a) {
+    print("test_json_encode: ");
     for ($i = 0; $i < 1000; ++$i) {
-        $s = var_export($a, True);
-        eval('$u='.$s.';');
+        $j = json_encode($a);
     }
+    return $j;
 }
 
-function bench($f, $a) {
-    $__f = create_function('$a', $f.'($a);');
+function test_json_decode($j) {
+    print("test_json_decode: ");
+    for ($i = 0; $i < 1000; ++$i) {
+        $a = json_decode($j, True);
+    }
+    return $a;
+}
+
+function bench_f($f, $a) {
+    $__f = create_function('$a', 'return '.$f.'($a);');
     $start = gettimeofday(True);
-    $__f($a);
+    $r = $__f($a);
     $past = gettimeofday(True) - $start;
     printf("%f ms\n", $past);
+    return $r;
+}
+
+function bench_integer_array($keys, $size) {
+    print("\ninteger, value size: $size\n");
+    $a = generate_random_integer_array($keys, $size);
+    $s = bench_f('test_serialize', $a);
+    $r = bench_f('test_unserialize', $s);
+    $j = bench_f('test_json_encode', $a);
+    $r = bench_f('test_json_decode', $j);
+}
+
+function bench_string_array($keys, $length) {
+    print("\nstring, value length: $length\n");
+    $a = generate_random_string_array($keys, $length);
+    $s = bench_f('test_serialize', $a);
+    $r = bench_f('test_unserialize', $s);
+    $j = bench_f('test_json_encode', $a);
+    $r = bench_f('test_json_decode', $j);
 }
 
 try {
 
-    print("\nvalue length: 100\n");
-    $a = generate_array(100, 100);
-    bench('test_serialize', $a);
-    bench('test_json', $a);
-    bench('test_var_export', $a);
+    bench_integer_array(10, 100);
+    bench_integer_array(100, 10000);
+    bench_integer_array(1000, 1000000);
 
-    print("\nvalue length: 1000\n");
-    $a = generate_array(100, 1000);
-    bench('test_serialize', $a);
-    bench('test_json', $a);
-    bench('test_var_export', $a);
+    bench_string_array(10, 10);
+    bench_string_array(100, 100);
+    bench_string_array(1000, 100);
+    bench_string_array(100, 1000);
 
 } catch(Exception $e) {
 
