9f1d2d7a78c82c5a35434bdf4e4169e9ef18a4fe
firewood <karamaki@gmail.com>
2015-05-15 17:06:04 +0900

    revise makefile

--- hello_gen.mk
+++ hello_gen.mk
@@ -1,14 +1,31 @@
 
-hello: char
-	./char > _.c
-	$(CC) -c -Wa,-R -w _.c
+#hello: char
+#	./char > _.c
+#	$(CC) -c -Wa,-R -w _.c
+#	$(LD) _.o -e _ -o $@
+#	./$@
+
+#char: chars.cc char.cc
+#	./hello_gen > chars.cc
+#	$(CXX) char.cc -o char
+
+
+default: hello
+	./$<
+
+hello: _.c
+	$(CC) -c -Wa,-R -w $<
 	$(LD) _.o -e _ -o $@
-	./$@
 
-char: chars.cc char.cc
-	./hello_gen > chars.cc
-	$(CXX) char.cc -o char
+_.c: hello_gen
+	./$< > $@
 
-hello_gen: hello_gen.cc
+hello_gen: hello_gen.cc chars.cc
+	$(CXX) -o $@ $<
+
+chars.cc: hello_gen_gen
+	./$< > $@
+
+hello_gen_gen: hello_gen_gen.cc
 	$(CXX) -fno-operator-names -o $@ $<
 
