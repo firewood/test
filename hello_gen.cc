354ac6fd7f8f1a66a8fe9fb27846c32c57260386
firewood <karamaki@gmail.com>
2015-05-15 17:05:15 +0900

    rename

--- hello_gen.cc
+++ hello_gen.cc
@@ -1,52 +1,31 @@
-#include <string>
 #include <iostream>
-#include <cstring>
-#define XBYAK32
-#include "xbyak/xbyak.h"
+#include <string>
 
-class PutString: public Xbyak::CodeGenerator {
-	void syscall() { db(0x0F); db(0x05); }
-	void int80h() { db(0xCD); db(0x80); }
-public:
-	PutString(const std::string &message) {
-		unsigned int *data = (unsigned int *)message.data();
-		mov(dword[esp - 16], data[0]);
-		mov(dword[esp - 12], data[1]);
-		mov(dword[esp - 8], data[2]);
-		mov(word[esp - 4], data[3]);
-		mov(edx, message.length());
-		dec(eax);
-		mov(ebx, 1);
-		jmp("@f");
-		add(byte[eax], al);
-		dec(eax);
-		lea(esi, ptr[esp - 16]);
-		mov(edi, 1);
-		mov(eax, edi);
-		syscall();
-		xor(edi, edi);
-		mov(eax, 60);
-		syscall();
-L("@@");
-		lea(ecx, ptr[esp - 16]);
-		mov(eax, 4);
-		int80h();
-		xor(ebx, ebx);
-		mov(eax, 1);
-		int80h();
+using namespace std;
+
+template <unsigned int n> struct Char;
+
+template <> struct Char<0> {
+	operator string() { return "!'_'"; }
+};
+
+template <> struct Char<1> {
+	operator string() { return "'!'-' '"; }
+};
+
+template <unsigned int n> struct Char {
+	operator string() {
+		if (n & 1) {
+			return "((" + string(Char<(n>>1)>()) + ")<<(" + string(Char<1>()) + "))|" + string(Char<1>());
+		}
+		return "(" + string(Char<(n>>1)>()) + ")<<(" + string(Char<1>()) + ")";
 	}
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-	PutString put_string("Hello, world!\n");
-	unsigned int *bin = put_string.getCode<unsigned int *>();
-	size_t dwords = (put_string.getSize() + 3) / 4;
-	std::string delim = "";
-	for (size_t i = 0; i < dwords; ++i) {
-		std::cout << "cout << " + delim + "string(Char<" << std::dec << bin[i] << ">()) << endl;";
-		std::cout << "  // " << std::hex << bin[i];
-		std::cout << std::endl;
-		delim = "\",\" + ";
-	}
+	cout << "_[]={" << endl;
+#include "chars.cc"
+	cout << "};" << endl;
+	return 0;
 }
---
+++ hello_gen_gen.cc
@@ -0,0 +1,52 @@
+#include <string>
+#include <iostream>
+#include <cstring>
+#define XBYAK32
+#include "xbyak/xbyak.h"
+
+class PutString: public Xbyak::CodeGenerator {
+	void syscall() { db(0x0F); db(0x05); }
+	void int80h() { db(0xCD); db(0x80); }
+public:
+	PutString(const std::string &message) {
+		unsigned int *data = (unsigned int *)message.data();
+		mov(dword[esp - 16], data[0]);
+		mov(dword[esp - 12], data[1]);
+		mov(dword[esp - 8], data[2]);
+		mov(word[esp - 4], data[3]);
+		mov(edx, message.length());
+		dec(eax);
+		mov(ebx, 1);
+		jmp("@f");
+		add(byte[eax], al);
+		dec(eax);
+		lea(esi, ptr[esp - 16]);
+		mov(edi, 1);
+		mov(eax, edi);
+		syscall();
+		xor(edi, edi);
+		mov(eax, 60);
+		syscall();
+L("@@");
+		lea(ecx, ptr[esp - 16]);
+		mov(eax, 4);
+		int80h();
+		xor(ebx, ebx);
+		mov(eax, 1);
+		int80h();
+	}
+};
+
+int main(int argc, char * argv[])
+{
+	PutString put_string("Hello, world!\n");
+	unsigned int *bin = put_string.getCode<unsigned int *>();
+	size_t dwords = (put_string.getSize() + 3) / 4;
+	std::string delim = "";
+	for (size_t i = 0; i < dwords; ++i) {
+		std::cout << "cout << " + delim + "string(Char<" << std::dec << bin[i] << ">()) << endl;";
+		std::cout << "  // " << std::hex << bin[i];
+		std::cout << std::endl;
+		delim = "\",\" + ";
+	}
+}
